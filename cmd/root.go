c40728d1fa1c960d48db7efec619f608c3457876
John Brunton <john_brunton@hotmail.co.uk>
2020-06-23 21:44:24 +0100

    remove viper and config

--- cmd/root.go
+++ cmd/root.go
@@ -18,13 +18,10 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 
 	"github.com/jbrunton/g3ops/cmd/context"
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -54,7 +51,7 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	//cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -67,39 +64,4 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	rootCmd.AddCommand(context.ContextCmd)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".g3ops" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".g3ops")
-		viper.SetConfigType("json")
-		cfgFile = filepath.Join(home, ".g3ops")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	} else {
-		fmt.Println(err)
-		fmt.Println("Creating config file:", cfgFile)
-		viper.SafeWriteConfigAs(cfgFile)
-	}
-
-	// contexts := viper.GetString("test")
-	// fmt.Println("contexts:", contexts)
 }
