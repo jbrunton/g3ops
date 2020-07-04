6e25aee525cce644eb69ffb4531695d16f4b65a1
John Brunton <john_brunton@hotmail.co.uk>
2020-07-04 22:33:47 +0100

    G3OPS-13 fix compilation errors

--- lib/workflow_validator_test.go
+++ lib/workflow_validator_test.go
@@ -10,18 +10,18 @@ import (
 func setupValidator(templateContent string) (*afero.Afero, *WorkflowValidator, *WorkflowDefinition) {
 	fs := CreateMemFs()
 	WorkflowDefinition := newTestWorkflowDefinition("test", templateContent)
-	validator := newWorkflowValidator(fs)
+	validator := NewWorkflowValidator(fs)
 	return fs, validator, WorkflowDefinition
 }
 
 func TestValidateContent(t *testing.T) {
 	fs, validator, definition := setupValidator(exampleTemplate)
 
-	fs.WriteFile(definition.destination, []byte(exampleTemplate), 0644)
+	fs.WriteFile(definition.Destination, []byte(exampleTemplate), 0644)
 	result := validator.ValidateContent(definition)
 
-	assert.True(t, result.valid)
-	assert.Equal(t, []string{}, result.errors)
+	assert.True(t, result.Valid)
+	assert.Equal(t, []string{}, result.Errors)
 }
 
 func TestValidateContentMissing(t *testing.T) {
@@ -29,18 +29,18 @@ func TestValidateContentMissing(t *testing.T) {
 
 	result := validator.ValidateContent(definition)
 
-	assert.False(t, result.valid)
-	assert.Equal(t, []string{"Workflow missing for \"test\" (expected workflow at .github/workflows/test.yml)"}, result.errors)
+	assert.False(t, result.Valid)
+	assert.Equal(t, []string{"Workflow missing for \"test\" (expected workflow at .github/workflows/test.yml)"}, result.Errors)
 }
 
 func TestValidateContentOutOfDate(t *testing.T) {
 	fs, validator, definition := setupValidator(exampleTemplate)
 
-	fs.WriteFile(definition.destination, []byte("incorrect content"), 0644)
+	fs.WriteFile(definition.Destination, []byte("incorrect content"), 0644)
 	result := validator.ValidateContent(definition)
 
-	assert.False(t, result.valid)
-	assert.Equal(t, []string{"Content is out of date for \"test\" (.github/workflows/test.yml)"}, result.errors)
+	assert.False(t, result.Valid)
+	assert.Equal(t, []string{"Content is out of date for \"test\" (.github/workflows/test.yml)"}, result.Errors)
 }
 
 func TestValidateSchema(t *testing.T) {
@@ -48,8 +48,8 @@ func TestValidateSchema(t *testing.T) {
 
 	result := validator.ValidateSchema(definition)
 
-	assert.True(t, result.valid)
-	assert.Equal(t, []string{}, result.errors)
+	assert.True(t, result.Valid)
+	assert.Equal(t, []string{}, result.Errors)
 }
 
 func TestValidateSchemaMissingField(t *testing.T) {
@@ -57,6 +57,6 @@ func TestValidateSchemaMissingField(t *testing.T) {
 
 	result := validator.ValidateSchema(definition)
 
-	assert.False(t, result.valid)
-	assert.Equal(t, []string{"(root): jobs is required"}, result.errors)
+	assert.False(t, result.Valid)
+	assert.Equal(t, []string{"(root): jobs is required"}, result.Errors)
 }
