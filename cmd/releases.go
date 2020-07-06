ae297d5324557dae7fb2085b24ec03bea8883513
John Brunton <john_brunton@hotmail.co.uk>
2020-07-06 11:04:17 +0100

    G3OPS-17 list releases

---
+++ cmd/releases.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+
+	"github.com/google/go-github/github"
+	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+)
+
+func newListReleasesCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "ls",
+		Short: "Lists services in the current context",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := context.Background()
+			token := os.Getenv("GITHUB_TOKEN")
+			var client *github.Client
+			if token != "" {
+				ts := oauth2.StaticTokenSource(
+					&oauth2.Token{AccessToken: token},
+				)
+				tc := oauth2.NewClient(ctx, ts)
+				client = github.NewClient(tc)
+			} else {
+				fmt.Println("Warning: no GITHUB_TOKEN set. g3ops won't be able to authenticate, and some functionality won't be supported.")
+				client = github.NewClient(nil)
+			}
+
+			releases, _, err := client.Repositories.ListReleases(context.Background(), "jbrunton", "g3ops", nil)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"Name", "Status"})
+			for _, release := range releases {
+				table.SetColumnColor(
+					tablewriter.Colors{tablewriter.FgGreenColor},
+					tablewriter.Colors{tablewriter.FgYellowColor},
+				)
+				var status string
+				if *release.Draft {
+					status = "DRAFT"
+				} else {
+					status = "PUBLISHED"
+				}
+
+				row := []string{*release.Name, status}
+				table.Append(row)
+			}
+			table.Render()
+		},
+	}
+	return cmd
+}
+
+func newReleasesCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "releases",
+	}
+	cmd.AddCommand(newListReleasesCmd())
+	return cmd
+}
