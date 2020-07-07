fd1b582661077212edbd4fc7622ff491d7caaa10
John Brunton <john_brunton@hotmail.co.uk>
2020-07-07 15:46:10 +0100

    G3OPS-34 fix crash creating release

--- lib/container.go
+++ lib/container.go
@@ -13,20 +13,12 @@ type Container struct {
 	Clock         Clock
 }
 
-// Copy - creates a copy of the container
-// func (container *Container) Copy() *Container {
-// 	return &Container{
-// 		FileSystem:    container.FileSystem,
-// 		Executor:      container.Executor,
-// 		GitHubService: container.GitHubService,
-// 	}
-// }
-
 // NewContainer - creates a new production container instance. Use NewTestContainer for testing.
 func NewContainer() *Container {
 	return &Container{
 		FileSystem:    CreateOsFs(),
 		Executor:      &CommandExecutor{},
 		GitHubService: services.NewGitHubService(),
+		Clock:         NewSystemClock(),
 	}
 }
